cf6f9c99eb390e1ba60321304c705772ee6fd094
OriginalSin <saleks@scanex.ru>
2011-08-02 13:59:14 +0400

    Проверка доступности изображения к работе с пикселами

--- src-as3/RasterTile.hx
+++ src-as3/RasterTile.hx
@@ -131,15 +131,14 @@ class RasterTile
 			updateAlphaListener = function(event) { me.updateAlpha(); }
 			contents.addEventListener(Event.ENTER_FRAME, updateAlphaListener);
 
-			var size = 32;
-			var bmp = new BitmapData(size, size, true, 0);
-			try {
+			if(contents.loaderInfo.parentAllowsChild) {
+				var size = 32;
+				var bmp = new BitmapData(size, size, true, 0);
 				bmp.draw(contents);
-			} catch (e:Error) {}
-			var hist = bmp.histogram();
-			if (hist[3][255] != 1024) isOverlay = true;		// по гистограмме определяем тайлы где в верхнем левом углу 32х32 все alpha = 0xFF
-			bmp.dispose();
-
+				var hist = bmp.histogram();
+				if (hist[3][255] != 1024) isOverlay = true;		// по гистограмме определяем тайлы где в верхнем левом углу 32х32 все alpha = 0xFF
+				bmp.dispose();
+			}
 			if (isOverlay && isReplacement)
 				remove();
 		}
