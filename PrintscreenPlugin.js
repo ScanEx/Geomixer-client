29cb8abcc1c7989ee6bf07fdb1579b7825118612
Alexander Parshin <parshin@scanex.ru>
2011-09-09 16:44:59 +0400

    Использование стандратного скрипта ImgSave.ashx для сохранения картинок

--- PrintscreenPlugin.js
+++ PrintscreenPlugin.js
@@ -10,7 +10,7 @@ _translationsHash.addtext("eng", {
 	"screenshotPlugin.title" : "Preview"
 });
 
-var _pluginParams = {serverURL: "http://dev2.kosmosnimki.ru/TestPNG.ashx"};
+//var _pluginParams = {serverHost: "http://mapstest.kosmosnimki.ru/ImgSave.ashx"};
 
 var printScreeshot = function(url)
 {
@@ -21,22 +21,57 @@ var printScreeshot = function(url)
 	popup.print();
 }
 
+//почему-то стандартный метод для opera не работает, а этот не работает в chrome :)
+var printScreeshotOpera = function(url) {
+
+	function makepage(src) {
+	  return "<html>\n" +
+		"<head>\n" +
+		"<title>Temporary Printing Window</title>\n" +
+		"<script>\n" +
+		"function step1() {\n" +
+		"  setTimeout('step2()', 10);\n" +
+		"}\n" +
+		"function step2() {\n" +
+		"  window.print();\n" +
+		"  window.close();\n" +
+		"}\n" +
+		"</scr" + "ipt>\n" +
+		"</head>\n" +
+		"<body onLoad='step1()'>\n" +
+		"<img src='" + src + "'/>\n" +
+		"</body>\n" +
+		"</html>\n";
+	}
+
+  var link = "about:blank";
+  var pw = window.open(link,"_new");
+  pw.document.open();
+  pw.document.write(makepage(url));
+  pw.document.close();
+}
+
 var DEFAULT_WIDTH = 600;
 var makeScreeshot = function()
 {
-	var showScreenshot = function(pngUrl)
+	var showScreenshot = function(response)
 	{
+		response = JSON.parse(response);
+		
+		if (!parseResponse(response)) return;
+		
+		var pngUrl = serverBase + "ImgSave.ashx?id=" + response.Result;
+		
 		var image = new Image();
 		image.onload = function()
 		{
 			var h = DEFAULT_WIDTH/image.width*image.height;
 			var img = $("<img></img>").attr({src: pngUrl, width: DEFAULT_WIDTH, height: DEFAULT_WIDTH/image.width*image.height});
 			
-			// var btnPrint = $("<button></button>").text(_gtxt("screenshotPlugin.print")).click();
 			var btnPrint = makeButton(_gtxt("screenshotPlugin.print"));
 			btnPrint.onclick = function()
 			{
-				printScreeshot(pngUrl)
+				$.browser.opera ? printScreeshotOpera(pngUrl) : printScreeshot(pngUrl);
 			};
 			btnPrint.style.marginTop = "10px";
 			btnPrint.style.padding = "3px 9px";
@@ -45,7 +80,7 @@ var makeScreeshot = function()
 			//var tr1 = $("<tr></tr>").append($("<td align=center></td>").append($("<a></a>").attr({href: pngUrl, target: "_blank"}).append(img)));
 			var tr1 = $("<tr></tr>")
 				.append($("<td align=center></td>")
-					.append($("<a></a>").append(img).attr({href: "javascript:0"}).click(function()
+					.append($("<a></a>").append(img).css('cursor', 'pointer').click(function()
 						{
 							var popup = window.open();
 							popup.document.open();
@@ -54,6 +89,7 @@ var makeScreeshot = function()
 												 "<tr><td align='center'><button>"+ _gtxt("screenshotPlugin.print") +"</button></td></tr>" +
 												 "</table></body></html>");
 							popup.document.close();
+							$('button', popup.document).click(function(){ printScreeshot(pngUrl);} );
 						})
 					)
 				);
@@ -61,15 +97,15 @@ var makeScreeshot = function()
 			var tr2 = $("<tr></tr>").append($("<td align=center></td>").append(btnPrint));
 			var table = $("<table width='100%'></table>").append(tr1).append(tr2);
 			
-			//var div = $("<div></div>").append(table);
-			
 			var canvas = showDialog(_gtxt('screenshotPlugin.title'), table[0], DEFAULT_WIDTH+40, h+80);
 		}
 		image.src = pngUrl;
 	};
 	
 	// showScreenshot( "data:image/png;base64," + map.sendPNG({getBase64: true}).base64 );
-	map.sendPNG({url: _pluginParams.serverURL, func: showScreenshot});
+	
+	//имя передаём только ради разширения, из которого сервер вытащит тип картинки
+	map.sendPNG({url: serverBase + 'ImgSave.ashx?filename=a.png&WrapStyle=None', func: showScreenshot});
 }
 
 //PluginInterface
@@ -77,7 +113,7 @@ var beforeViewer = function(params){
 }
 
 var afterViewer = function(params){
-	_pluginParams = $.extend(_pluginParams, params);
+	//_pluginParams = $.extend(_pluginParams, params);
 
 	_iconPanel.changeCallcack('print', makeScreeshot);
 }
