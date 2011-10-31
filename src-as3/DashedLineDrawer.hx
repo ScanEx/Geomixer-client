c866d2531d3b9fc1e0c6a3a5e5fa67de4acd5fd9
OriginalSin <saleks@scanex.ru>
2011-10-31 21:00:31 +0400

    Сделал рефакторинг класса DashedLineDrawer https://papirus.net/t#id59385

--- src-as3/DashedLineDrawer.hx
+++ src-as3/DashedLineDrawer.hx
@@ -1,5 +1,6 @@
 import flash.display.Graphics;
 import flash.display.LineScaleMode;
+import flash.geom.Point;
 
 class DashedLineDrawer
 {
@@ -10,8 +11,6 @@ class DashedLineDrawer
 	var graphics:Graphics;
 	var outline:OutlineStyle;
 	var window:MapWindow;
-	var currentDashIndex:Int;
-	var currentDashLength:Float;
 	var totalDashesLength:Float;
 	var prop:Hash<String>;
 
@@ -34,7 +33,6 @@ class DashedLineDrawer
 		graphics.moveTo(x_, y_);
 		x = x_;
 		y = y_;
-		resetDashes();
 	}
 
 	function setLineStyle()
@@ -50,12 +48,6 @@ class DashedLineDrawer
 		graphics.lineStyle(Math.NaN, 0, 0);
 	}
 
-	function resetDashes()
-	{
-		currentDashIndex = 0;
-		currentDashLength = 0;
-	}
-
 	public function invisibleLineTo(x_, y_)
 	{
 		clearLineStyle();
@@ -63,7 +55,6 @@ class DashedLineDrawer
 		graphics.lineTo(x_, y_);
 		x = x_;
 		y = y_;
-		resetDashes();
 	}
 
 	public function lineTo(x_:Float, y_:Float)
@@ -79,114 +70,43 @@ class DashedLineDrawer
 		}
 		else
 		{
-			var dx = x_ - x;
-			var dy = y_ - y;
-			var len = Math.sqrt(dx*dx + dy*dy)/Math.abs(window.scaleY);
-
-			var x1 = x;
-			var y1 = y;
-			var x2 = x_;
-			var y2 = y_;
-			var t1:Float = 0;
-			var t2:Float = len;
-			var somethingLeft = true;
-			var clipSegment = function(a:Float, b:Float, c:Float)
-			{
-				if (somethingLeft)
-				{
-					var f1 = a*x1 + b*y1 + c;
-					var f2 = a*x2 + b*y2 + c;
-					if ((f1 < 0) && (f2 < 0))
-					{
-						somethingLeft = false;
-						return;
-					}
-					else if ((f1 > 0) && (f2 > 0))
-						return;
-					else
-					{
-						var m = f1/(f1 - f2);
-						var xc = x1 + (x2 - x1)*m;
-						var yc = y1 + (y2 - y1)*m;
-						var tc = t1 + (t2 - t1)*m;
-						if (f1 < 0)
-						{
-							x1 = xc;
-							y1 = yc;
-							t1 = tc;
-						}
-						else
-						{
-							x2 = xc;
-							y2 = yc;
-							t2 = tc;
-						}
-					}
-				}
+			var subLineExt:Extent = new Extent();
+			subLineExt.update(x, y);
+			subLineExt.update(x_, y_);
+			var contains = window.visibleExtent.overlaps(subLineExt);	// Линия в области видимости
+			if (!contains) {
+				invisibleLineTo(x_, y_);
 			}
-			var e = window.visibleExtent;
-			clipSegment(1, 0, e.minx);
-			clipSegment(-1, 0, e.maxx);
-			clipSegment(0, 1, e.miny);
-			clipSegment(0, -1, e.maxy);
-			if (!somethingLeft)
-				skipDashes(len, x_, y_);
 			else
 			{
-				if (t1 > 0)
-					skipDashes(t1, x1, y1);
-				var t = t2 - t1;
-				var x__ = x1;
-				var y__ = y1;
-				var xmult = (x2 - x1)/t;
-				var ymult = (y2 - y1)/t;
-				while (true)
-				{
-					if (currentDashIndex%2 == 0)
-						setLineStyle();
-					else
-						clearLineStyle();
-					var dist = outline.dashes[currentDashIndex] - currentDashLength;
-					if (t > dist)
-					{
-						x__ += xmult*dist;
-						y__ += ymult*dist;
-						t -= dist;
-						incrementDashIndex();
-						graphics.lineTo(x__, y__);
-					}
-					else
-					{
-						currentDashLength += t;
-						graphics.lineTo(x2, y2);
-						break;
-					}
-				}
-				if (t2 < len)
-					skipDashes(len - t2, x_, y_);
+				putLine(x_, y_);				
 			}
 		}
 		x = x_;
 		y = y_;
 	}
 
-	function skipDashes(t:Float, x_:Float, y_:Float)
+	function putLine(x_:Float, y_:Float)
 	{
-		t %= totalDashesLength;
-		while (t >= outline.dashes[currentDashIndex] - currentDashLength)
-		{
-			t -= outline.dashes[currentDashIndex] - currentDashLength;
-			incrementDashIndex();
+		if(totalDashesLength > 0) {		// Требуется отрисовка dash линии 
+			var pb:Point = new Point(x, y);
+			var pe:Point = new Point(x_, y_);
+			var sc = Math.abs(window.scaleY);
+			var len = Point.distance(pb, pe);
+			var count:Int = Std.int(len / (totalDashesLength*sc));
+			var dist:Float = 0;
+			for (i in 0...count) {
+				for (j in 0...Std.int(outline.dashes.length))
+				{
+					dist += outline.dashes[j] * sc;
+					var pt:Point = Point.interpolate(pb, pe, 1 - dist/len);
+					if (j % 2 == 0)		setLineStyle();
+					else				clearLineStyle();
+					graphics.lineTo(pt.x, pt.y);
+				}
+			}
 		}
-		currentDashLength += t;
+		setLineStyle();
 		graphics.lineTo(x_, y_);
-	}
-
-	function incrementDashIndex()
-	{
-		currentDashIndex += 1;
-		if (currentDashIndex == outline.dashes.length)
-			currentDashIndex = 0;
-		currentDashLength = 0;
 	}
 }
