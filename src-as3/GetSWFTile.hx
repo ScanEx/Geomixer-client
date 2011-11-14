ac6d9a4c73cb390ba877e9ff763140d19fe71706
OriginalSin <saleks@scanex.ru>
2011-11-14 20:59:34 +0400

    Поправка загрузки мультивременных тайлов

--- src-as3/GetSWFTile.hx
+++ src-as3/GetSWFTile.hx
@@ -1,67 +1,34 @@
 import flash.events.Event;
-import flash.utils.Timer;
-import flash.events.TimerEvent;
 	
 class GetSWFTile
 {
 	private var onLoad:Array<Dynamic>->Void;
 	private var links:Array<String>;
 	private var data:Array<Dynamic>;
-	private var loaders:Array<GetSWFFile>;
 
-	private var myTimer:Timer;
-	private static var reqCount:Int = 0;
-	private static var reqLimit:Int = 8;
+	private var count:Int;
 	
 	public function new(links_:Array<String>, onLoad_:Array<Dynamic>->Void)
 	{
 		links = links_;
 		onLoad = onLoad_;
 		init();
-		//trace('--------linkslinkslinks ---------- ' + links.length + ' : ' );
+		count = links.length;
 	}
 
 	private function init()
 	{
 		data = new Array<Dynamic>();
-		loaders = new Array<GetSWFFile>();
-		myTimer = null;
-		myTimer = new Timer(10);
-		myTimer.addEventListener("timer", getNext);
-		myTimer.start();
-	}
-
-	public function addMember(arr:Array<Dynamic>)
-	{
-		if (arr != null) {
-			data = data.concat(arr);
-		}
-		arr = null;
-
-		reqCount--;
-		//trace('------------------ ' + reqCount + ' : ' + links.length);
-		if (links.length < 1) 
-		{
-			onLoad(data);
-			//data = null;
-			//for (loader in loaders) loader = null;
-			//loaders = null;
-			if(myTimer != null) {
-				myTimer.stop();
-				if (myTimer.hasEventListener("timer")) myTimer.removeEventListener("timer", getNext);
-				return;
-			}
+		var me = this;
+		for(url in links) {
+			new GetSWFFile(url, function(arr:Array<Dynamic>) {
+				me.count--;
+				//for (it in arr) me.data.push(it);
+				me.data = me.data.concat(arr);
+				if (me.count < 1) {
+					me.onLoad(me.data);
+				}
+			});
 		}
 	}
-
-	private function getNext(?event:TimerEvent)
-	{
-		
-		//trace('nnnnnnnnnnn ' + reqCount + ' : ' + reqLimit + ' : ' + links.length + ' : ' + myTimer.currentCount);
-		if (reqCount > reqLimit || links.length < 1) return;
-		var url:String = links.shift();
-		reqCount++;
-		loaders.push(new GetSWFFile(url, addMember));
-	}
-	
 }
