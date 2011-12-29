a1063b74241f951fa6db7a8290085ebae07a8e3e
MLobanov <mlobanov@scanex.ru>
2011-12-29 20:34:47 +0400

    Багфикс с отсутсвием проверки на нулл у сессии Новый плашин - корзина заказов

--- plugins/Cart.js
+++ plugins/Cart.js
@@ -1,31 +1,33 @@
-﻿(function($){
+﻿(function($, oMap){
 	var pluginPath;
 	_translationsHash.addtext("rus", {
 		"Корзина": "Корзина",
 		"Отправить заказ": "Отправить заказ",
-		"Общая сумма:": "Общая сумма:"
+		"Общая сумма:": "Общая сумма:",
+		"Корзина пуста": "Корзина пуста"
 	});
 	_translationsHash.addtext("eng", {
 		"Корзина": "Cart",
 		"Отправить заказ": "Send order",
-		"Общая сумма:": "Total price:"
+		"Общая сумма:": "Total price:",
+		"Корзина пуста": "Cart is empty"
 	});
 	
-	var arrMapObjects = {};
+	var arrCartObjects = {};
 	var oCartDiv = _div(null, [['attr', 'Title', _gtxt("Корзина")]]);
 	var oItemList = _div();
 	var oItemSummary = _div();
 	var oCommentInput = _textarea(null, [['css', 'width', '100%']]);
 
-	var oSendButton = _a([_t(_gtxt('Отправить заказ'))], [['attr', 'href', 'mailto:sales@scanex.ru']]);
+	var oSendButton = makeLinkButton(_gtxt('Отправить заказ'));
 	_(oCartDiv, [oItemList, oItemSummary, _br(), oCommentInput, _br(), oSendButton]);
 		
 	var drawRow = function(id){
-		var oMapObject = arrMapObjects[id].mapObject;
-		var price = arrMapObjects[id].price;
+		var title = arrCartObjects[id].title;
+		var price = arrCartObjects[id].price;
 		
 		var oContainer = _div(null, [['css', 'position', 'relative']]);
-		var oItem = _span([_t(oMapObject.properties.scene_id)]);
+		var oItem = makeLinkButton(title);
 		var oPrice = _span([_t("$" + price.toString())], [['css', 'position', 'absolute'], ['css', 'right', '15px']]);
 		
 		var remove = makeImageButton('img/closemin.png','img/close_orange.png');
@@ -36,22 +38,28 @@
 		_(oContainer, [oItem, oPrice, remove]);
 		_(oItemList, [oContainer]);
 		remove.onclick = function(){
-			delete arrMapObjects[id];
+			delete arrCartObjects[id];
 			drawList();
+		}
+		oItem.onclick = function(){
+			var oExtent = getBounds(arrCartObjects[id].geometry.coordinates);
+			oMap.zoomToExtent(oExtent.minX, oExtent.minY, oExtent.maxX, oExtent.maxY);
 		}
 	}
 
 	var totalPrice = 0;
 	var idsString = "";
 	
-	var updateSendLink = function(){
-		var sSubject = escape("Ikonos_pikkolo order");
-		var sBody = escape("Login: " + nsGmx.AuthManager.getUserName() + "\nComment:\n" + oCommentInput.value);
-		if (idsString != ""){sBody += escape("\nscene_ids: " + idsString);}
+	oSendButton.onclick = function(){
+		if (idsString == ""){alert("Корзина пуста"); return;}
+		_mapHelper.createPermalink(function(id){
+			var permalinkUrl = "http://" + window.location.host + window.location.pathname + "?permalink=" + id;
+			var EMail = nsGmx.AuthManager.isAccounts() ? escape(nsGmx.AuthManager.getUserName()) : escape("sales@scanex.ru");
+			Url = "http://search.kosmosnimki.ru/CreateOrder.ashx?TinyReference=" + escape(permalinkUrl) + "&Surname=" + escape(nsGmx.AuthManager.getUserName()) + "&Name=TilesOrder&Organization=TilesOrder&Email=" + EMail +"&Phone=TilesOrder&Comment=" + escape(oCommentInput.value) + "&ReceiveWay=Tiles";
+			location.replace(Url);
+		});
 		
-		oSendButton.href = "mailto:sales@scanex.ru?subject=" + sSubject + "&body=" + sBody;
 	}
-	oCommentInput.onblur = updateSendLink;
 
 	var drawList = function(){
 		totalPrice = 0;
@@ -59,9 +67,9 @@
 		removeChilds(oItemList);
 		removeChilds(oItemSummary);
 		
-		for (var id in arrMapObjects){
+		for (var id in arrCartObjects){
 			drawRow(id);
-			totalPrice += arrMapObjects[id].price;
+			totalPrice += arrCartObjects[id].price;
 			idsString += id + ";";
 		}
 		if (totalPrice > 0){
@@ -70,15 +78,12 @@
 			oSummary.style.borderTop = '1px solid';
 			_(oItemSummary, [oSummary]);
 		}
-		updateSendLink();
 	}
 	
-	var addToCart = function(oMapObject){
-		if (oMapObject.properties.scene_id in arrMapObjects) return;
-		arrMapObjects[oMapObject.properties.scene_id] = {
-			mapObject: oMapObject,
-			price: Math.round(oMapObject.getArea()/10000)/100
-		}
+	var addToCart = function(oCartObject){
+		if (oCartObject.title in arrCartObjects) return;
+		arrCartObjects[oCartObject.title] = oCartObject;
+
 		drawList();
 		loadMenu();
 	}
@@ -97,9 +102,15 @@
 		addToCart: addToCart
 	}
 
+	_mapHelper.customParamsManager.addProvider({
+		name: 'Cart',
+		loadState: function(state) { for (var oCartObject in state.objectsInCart) { addToCart(state.objectsInCart[oCartObject]);} },
+		saveState: function() { return { objectsInCart: arrCartObjects }; }
+	});
+				
 	gmxCore.addModule("Cart", publicInterface, {init: function(module, path)
 		{
 			pluginPath = path;
 		}
 	});
-})(jQuery)
+})(jQuery, globalFlashMap)
