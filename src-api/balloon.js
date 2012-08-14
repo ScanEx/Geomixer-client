6e5da1de7dda21afbbc3bbb2bdaee355fe3e383a
OriginalSin <saleks@scanex.ru>
2012-08-14 11:02:37 +0400

    Поправки балунов для полных заголовков DOCTYPE <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="ru-RU">

--- src-api/balloon.js
+++ src-api/balloon.js
@@ -428,7 +428,7 @@
 			div.appendChild(balloon);
 
 			var css = {
-				'table': 'margin: 0px; border-collapse: collapse; font-size: 16px; margin: 2px;',
+				'table': 'margin: 2px; border-collapse: collapse; font-size: 16px;',
 				'bg_top_left': 'background-color: transparent; width: 13px; height: 18px; border: 0px none; padding: 1px; display: block; background-position: 2px 9px; background-image: url(\''+apiBase+'img/tooltip-top-left.png\'); background-repeat: no-repeat;',
 				'bg_top': 'background-color: transparent; height: 18px; border: 0px none; padding: 0px; background-position: center 9px; background-image: url(\''+apiBase+'img/tooltip-top.png\'); background-repeat: repeat-x;',
 				'bg_top_right': 'background-color: transparent; width: 18px; height: 18px; border: 0px none; padding: 1px; display: block; background-position: -5px 9px; background-image: url(\''+apiBase+'img/tooltip-top-right.png\'); background-repeat: no-repeat;',
@@ -473,7 +473,7 @@
 				bottom: "-21px",
 				right: "15px"
 			};
-			if(document.doctype && (gmxAPI.isChrome || gmxAPI.isSafari || gmxAPI.isIE)) imgStyle["bottom"] = "-19px";
+			if(document.doctype && (gmxAPI.isChrome || gmxAPI.isSafari || gmxAPI.isIE)) imgStyle["bottom"] = (document.doctype.systemId ? "-19px" : "-17px");
 			var leg = gmxAPI.newElement("img",
 				{
 					src: apiBase + "img/tooltip-leg.png"
