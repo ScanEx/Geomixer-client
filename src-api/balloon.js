11c6b2914123baaf192e1109d6af39b57336b0ec
OriginalSin <saleks@scanex.ru>
2012-08-14 09:39:01 +0400

    Поправки балунов для DOCTYPE

--- src-api/balloon.js
+++ src-api/balloon.js
@@ -428,7 +428,7 @@
 			div.appendChild(balloon);
 
 			var css = {
-				'table': 'margin: 0px; border-collapse: collapse; font-size: 16px;',
+				'table': 'margin: 0px; border-collapse: collapse; font-size: 16px; margin: 2px;',
 				'bg_top_left': 'background-color: transparent; width: 13px; height: 18px; border: 0px none; padding: 1px; display: block; background-position: 2px 9px; background-image: url(\''+apiBase+'img/tooltip-top-left.png\'); background-repeat: no-repeat;',
 				'bg_top': 'background-color: transparent; height: 18px; border: 0px none; padding: 0px; background-position: center 9px; background-image: url(\''+apiBase+'img/tooltip-top.png\'); background-repeat: repeat-x;',
 				'bg_top_right': 'background-color: transparent; width: 18px; height: 18px; border: 0px none; padding: 1px; display: block; background-position: -5px 9px; background-image: url(\''+apiBase+'img/tooltip-top-right.png\'); background-repeat: no-repeat;',
@@ -470,7 +470,7 @@
 			
 			var imgStyle =	{
 				position: "absolute",
-				bottom: "-23px",
+				bottom: "-21px",
 				right: "15px"
 			};
 			if(document.doctype && (gmxAPI.isChrome || gmxAPI.isSafari || gmxAPI.isIE)) imgStyle["bottom"] = "-19px";
