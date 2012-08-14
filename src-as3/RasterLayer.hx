95f9618ebef43722f7ddb74e2d98b817f7628a2d
OriginalSin <saleks@scanex.ru>
2012-08-14 15:51:30 +0400

    во время рисования линии таскаем карту https://papirus.net/t#id161453

--- src-as3/RasterLayer.hx
+++ src-as3/RasterLayer.hx
@@ -45,7 +45,7 @@ class RasterLayer extends MaskedContent
 
 	public override function repaint()
 	{
-		if (Main.isDrawing || Main.isFluidZoom) return;		// В режиме рисования ничего не делаем
+		if (Main.isFluidZoom) return;		// В режиме zoom ничего не делаем
 		super.repaint();
 
 		var window = mapNode.window;
