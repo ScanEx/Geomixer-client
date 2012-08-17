3c4b11eff5c88cc3e07cc3f18aa8b09e66fbef50
OriginalSin <saleks@scanex.ru>
2012-08-18 00:06:09 +0400

    Поправка при кешировании растровых тайлов https://papirus.net/t#id151505

--- src-as3/Utils.hx
+++ src-as3/Utils.hx
@@ -169,6 +169,81 @@ class Utils
 	}
 
 	// Загрузить Image если возможно закешировать BitmapData + определение isOverlay
+	public static function loadCacheImage(url:String, onLoad:Dynamic->Void, ?onError:Void->Void, ?zxy:String, ?flagCache:Bool)
+	{
+		if (flagCache && displayObjectCache.exists(url)) {
+			var imgData:Dynamic = displayObjectCache.get(url);
+			imgData.loader = new Bitmap(imgData.bmp);
+			onLoad(imgData);
+			imgData.loader = null;
+			return;
+		}
+		var loader = new Loader();
+		var callOnError = function()
+		{
+			if (onError != null)
+				onError();
+		}
+		var timer = new Timer(60000, 1);
+		timer.addEventListener("timer", function(e:TimerEvent)
+		{
+			try { loader.close(); } catch (e:Error) {}
+			callOnError();
+		});
+		timer.start();
+
+		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, function(event) 
+		{ 
+			timer.stop();
+			var imgData:Dynamic = { };
+			imgData.isOverlay = null;
+			imgData.loader = loader;
+
+			var flag:Bool = event.target.parentAllowsChild;
+			if(flag) {
+				var size = 32;
+				var bmp = new BitmapData(size, size, true, 0);
+				bmp.draw(loader);
+				var hist = bmp.histogram();
+				if (hist[3][255] != 1024) imgData.isOverlay = true;		// по гистограмме определяем тайлы где в верхнем левом углу 32х32 все alpha = 0xFF
+				bmp.dispose();
+			
+				if(flagCache) {
+					bmp = new BitmapData(Std.int(loader.width), Std.int(loader.height), true, 0);
+					bmp.draw(loader);
+					imgData.bmp = bmp;
+					imgData.loader = new Bitmap(bmp);
+					displayObjectCache.set(url, imgData);
+				}
+			} else {
+				imgData.isOverlay = true;
+			}
+			
+			onLoad(imgData);
+			imgData.loader = null;
+			Main.bumpFrameRate();
+		});
+		loader.contentLoaderInfo.addEventListener(IOErrorEvent.IO_ERROR, function(event)
+		{
+			timer.stop();
+			callOnError();
+		});
+		var loaderContext:LoaderContext = new LoaderContext();
+		loaderContext.checkPolicyFile = true;
+		try 
+		{
+			loader.load(new URLRequest(url), loaderContext);
+		}
+		catch (e:Error) 
+		{
+			trace("security error while loading " + url);
+			timer.stop();
+			callOnError();
+		}
+	}
+
+/*
+	// Загрузить Image если возможно закешировать BitmapData + определение isOverlay
 	public static function loadCacheImage(url:String, onLoad:Dynamic->Void, ?onError:Void->Void)
 	{
 		var req:ReqImg = { url: url, onLoad: onLoad, onError: onError };
@@ -261,7 +336,7 @@ class Utils
 			callOnError();
 		}
 	}
-
+*/
 	// Загрузить BitmapData по url или взять из Cache
 	public static function loadBitmapData(url:String, onLoad:BitmapData->Void, ?noCache_:Bool)
 	{
