b51cf14747d04020539e77d5ce0d8ec432dc57d8
Parshin Alexander <parshin@scanex.ru>
2012-10-19 15:43:39 +0400

    Зарефакторил 3D плагин

--- plugins/3DPlugin.js
+++ plugins/3DPlugin.js
@@ -1,37 +1,21 @@
 ﻿(function($){
 
 var publicInterface = {
+    pluginName: '3DDemo',
     afterViewer: function()
     {
-        var button3D = $('<div/>', {'class': 'plugin3D-button'}).text('3D').click(function()
-        {
-            var x = Math.round(merc_x(globalFlashMap.getX()));
-            var y = Math.round(merc_y(globalFlashMap.getY()));
-            window.open("http://kosmosnimki.ru/3d/index.html?x=" + x + "&y=" + y, '_blank');
-        });
-        
-        var background3D = $('<div/>', {'class': 'plugin3D-background'});
-        $(globalFlashMap.allControls.div).append(background3D).append(button3D);
+        gmxAPI.baseLayersTools.addTool('3DDemo', {
+            hint: '3D',
+            onClick: function()
+            {
+                var x = Math.round(merc_x(globalFlashMap.getX()));
+                var y = Math.round(merc_y(globalFlashMap.getY()));
+                window.open("http://kosmosnimki.ru/3d/index.html?x=" + x + "&y=" + y, '_blank');
+            }
+        })
     }
 }
 
-if ( typeof gmxCore !== 'undefined' )
-{
-	gmxCore.addModule('3DPlugin', publicInterface, 
-	{ init: function(module, path)
-		{
-			var doLoadCss = function()
-			{
-				path = path || window.gmxJSHost || "";
-				$.getCSS(path + "3DPlugin.css");
-			}
-			
-			if ('getCSS' in $)
-				doLoadCss();
-			else
-				$.getScript(path + "../jquery/jquery.getCSS.js", doLoadCss);
-		}
-	});
-}
+window.gmxCore && window.gmxCore.addModule('3DPlugin', publicInterface);
 
 })(jQuery);
