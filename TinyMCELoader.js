5533ff4405388aeee8f60e2984683ed12ac63a7b
Parshin Alexander <parshin@scanex.ru>
2013-02-26 19:21:40 +0400

    Совместимость со старой версией TinyMCE, которая используется в wiki

--- TinyMCELoader.js
+++ TinyMCELoader.js
@@ -8,6 +8,8 @@ gmxCore.addModule('TinyMCELoader', {}, {
                 plugins: 'fullscreen',
                 language: 'ru',
                 theme_advanced_buttons1: 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,|,link,unlink,fullscreen,code',
+                theme_advanced_buttons2: '',
+                theme_advanced_buttons3: '',
                 theme_advanced_statusbar_location: 'none',
                 theme_advanced_toolbar_location: 'bottom',
                 theme_advanced_toolbar_align: 'center'
