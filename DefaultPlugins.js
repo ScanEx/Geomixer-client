6383653d1d349b467e5bb31b18bab978a0c4eec1
Parshin Alexander <parshin@scanex.ru>
2013-05-20 15:19:50 +0400

    Добавил кадастровый плагин в исходники Геомиксера (единственный плагин по-умолчанию)

--- DefaultPlugins.js
+++ DefaultPlugins.js
@@ -1,5 +1,5 @@
 ﻿window.nsGmx = window.nsGmx || {};
 nsGmx._defaultPlugins = 
 [
-    {pluginName: 'Cadastre', file: 'plugins/cadastre.js', module: 'cadastre', mapPlugin: false, isPublic: true}
+    {pluginName: 'Cadastre', file: 'plugins/cadastre/cadastre.js', module: 'cadastre', mapPlugin: false, isPublic: true}
 ];
