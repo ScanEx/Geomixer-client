fc6bb7778aa2887621de614a870e9541e8ba1600
OriginalSin <saleks@scanex.ru>
2013-07-02 14:42:50 +0400

    Отключение fadeAnimation при наличии window.gmxPhantom

--- src-api/leafletProxy.js
+++ src-api/leafletProxy.js
@@ -4210,7 +4210,7 @@
 					,doubleClickZoomGMX: true
 					,attributionControl: false
 					,trackResize: true
-					,fadeAnimation: (window.isPhantom ? false : true)		// отключение fadeAnimation при запуске тестов
+					,fadeAnimation: (window.gmxPhantom ? false : true)		// отключение fadeAnimation при запуске тестов
 					//,zoomAnimation: false
 					//,boxZoom: false
 					//,zoomAnimation: (gmxAPI.isChrome ? false : true)
