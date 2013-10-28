730b20347432bdacede32d3f908431a8425843b5
OriginalSin <saleks@scanex.ru>
2013-10-28 20:15:12 +0400

    Скрыть балуны при click на карте

--- src-api/leafletProxy.js
+++ src-api/leafletProxy.js
@@ -4182,6 +4182,7 @@
 				attr['evName'] = 'onClick';
 				gmxAPI._leaflet['clickAttr'] = attr;
 				clickDone = gmxAPI._leaflet['utils'].chkGlobalEvent(attr);
+				if(!clickDone) gmxAPI._listeners.dispatchEvent('hideBalloons', gmxAPI.map, {});	// Проверка map Listeners на hideBalloons
 			};
 			gmxAPI._leaflet['chkClick'] = chkClick;
 			LMap.on('click', chkClick);
