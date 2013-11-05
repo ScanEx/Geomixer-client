708df7720ba46e0c9f1ff423ae20ad8a0ec3b285
OriginalSin <saleks@scanex.ru>
2013-11-05 14:34:07 +0400

    Для SPOT6 снимки без поворота

--- src-api/VectorLayer.js
+++ src-api/VectorLayer.js
@@ -1601,7 +1601,10 @@
 				}
 				return out;
 			}
-			var shiftPoints = chPoints([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]);
+			var shiftPoints = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]];
+            if(item.properties.PLATFORM !== 'SPOT 6') {
+                shiftPoints = chPoints([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]);
+            }
             var pt = gmxAPI._leaflet['ProjectiveImage']({
 					'imageObj': content
 					,'points': shiftPoints
