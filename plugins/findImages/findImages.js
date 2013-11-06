8384fceb74c768fe3f479c53ece743edd011b609
Alexander Parshin <parshin.alexander@gmail.com>
2013-11-06 11:50:47 +0400

    Обновление плагина для sales

--- plugins/findImages/findImages.js
+++ plugins/findImages/findImages.js
@@ -1,4 +1,38 @@
 ﻿(function() {
+    var server2object = function(fields, values) {
+        var item = {};
+        var props = {};
+        
+        for (var j = 0, len1 = values.length; j < len1; j++)
+        {
+            var fname = fields[j];
+            var it = values[j];
+            if (fname === 'geomixergeojson') {
+                item.geometry = gmxAPI.from_merc_geometry(it);
+            } else {
+                props[fname] = it;
+            }
+        }
+        
+        item.properties = props;
+        return item;
+    }
+    
+    var SceneCollection = function(layer, tableDataProvider) {
+        
+        this.add = function(items) {
+            layer.addItems(items);
+            tableDataProvider.addOriginalItems(items);
+        }
+        
+        this.remove = function(itemID) {
+            layer.removeItems([itemID]);
+            tableDataProvider.filterOriginalItems(function(item) {
+                return item.properties.ogc_fid !== itemID;
+            });
+        }
+    }
+            
     var publicInterface = {
         pluginName: 'findImages',
         afterViewer: function(params, map) {
@@ -7,33 +41,14 @@
                 return;
             }
             
-            var server2object = function(fields, values) {
-                var item = {};
-                var props = {};
-                
-                for (var j = 0, len1 = values.length; j < len1; j++)
-                {
-                    var fname = fields[j];
-                    var it = values[j];
-                    if (fname === 'geomixergeojson') {
-                        item.geometry = gmxAPI.from_merc_geometry(it);
-                    } else {
-                        props[fname] = it;
-                    }
-                }
-                
-                item.properties = props;
-                return item;
-            }
-            
             var layerNames = ['076EFE8A3D66461BBEC1234B006DE272', '378F08F3C00043528A70CE6878E7F487'];
             
             var selectedImagesLayer = map.addLayer({properties: {
                 IsRasterCatalog: false,
-                RCMinZoomForRasters: 1,
+                RCMinZoomForRasters: 8,
                 title: 'Express Sales Results',
                 styles: [{
-                    RenderStyle: {outline: {color: 0x0000ff, thickness: 3}, fill: {opacity: 0}}
+                    RenderStyle: {outline: {color: 0xff8800, thickness: 3}, fill: {opacity: 0}}
                 }]
             }});
             
@@ -43,6 +58,8 @@
                     if (!event.attr.ctrlKey) {
                         return;
                     }
+                    
+                    //отправляем запрос на сервер, чтобы получить полную геометрию
                     gmxAPI.sendCrossDomainPostRequest(
                         'http://maps.kosmosnimki.ru/VectorLayer/Search.ashx',
                         {
@@ -51,7 +68,6 @@
                             layer: layerName,
                             geometry: true,
                             query: '[ogc_fid]=' + event.obj.properties.ogc_fid
-                            //,'columns': "[{'Value': '[ogc_fid]'}, {'Value':'[Name]'}]"
                         },
                         function(response) {
                             if (!parseResponse(response)) {
@@ -60,7 +76,7 @@
                             
                             var newObj = server2object(response.Result.fields, response.Result.values[0]);
                             newObj.properties.ogc_fid = nsGmx.Utils.generateUniqueID();
-                            selectedImagesLayer.addItems([newObj]);
+                            sceneCollection.add([newObj]);
                         }
                     )
                 });
@@ -70,13 +86,16 @@
                 if (!event.attr.ctrlKey) {
                         return;
                 }
-                selectedImagesLayer.removeItems([event.obj.properties.ogc_fid]);
+                sceneCollection.remove(event.obj.properties.ogc_fid);
             })
 
-            var findImages = function() {
-                layerNames.forEach(getLayerImages);
-            }
-            var getLayerImages = function(layerName) {
+            // var findImages = function(geometry) {
+                // layerNames.forEach(function(layerName) {
+                    // getLayerImages();
+                // })
+            // }
+            
+            var getLayerImages = function(layerName, geometry) {
                 var out = [];
                 gmxAPI.sendCrossDomainPostRequest(
                     'http://maps.kosmosnimki.ru/VectorLayer/Search.ashx'
@@ -84,10 +103,9 @@
                         'WrapStyle': 'window'
                         ,'count': 'add'
                         ,'pagesize': 1000
-                        ,'layer': layerName			            // слой в котором ищем обьекты
-                        ,'border': JSON.stringify(geo)	        // геометрия
+                        ,'layer': layerName
+                        ,'border': JSON.stringify(gmxAPI.merc_geometry(geometry))
                         ,'geometry': true
-                        //,'columns': "[{'Value': '[ogc_fid]'}, {'Value':'[Name]'}]"
                     }
                     ,function(ph) {
                         var items = [];
@@ -104,7 +122,7 @@
                                 
                                 items.push(item);
                             }
-                            selectedImagesLayer.addItems(items);
+                            sceneCollection.add(items);
                         } else if (ph.Status == 'error') {
                             console.log(ph.ErrorInfo.ErrorMessage);
                         }
@@ -113,46 +131,61 @@
                 return out;
             }
 
-            var toolContainer = new map.ToolsContainer('findImages', {notSticky:1});
-            var tool = toolContainer.addTool('findImages', {
-                hint: 'Найти снимки'
-                ,onClick: findImages
-                ,onCancel: null
-            });
-            toolContainer.setVisible(false);
-                
-            var geo = null;
+            // var toolContainer = new map.ToolsContainer('findImages', {notSticky:1});
+            // var tool = toolContainer.addTool('findImages', {
+                // hint: 'Найти снимки'
+                // ,onClick: findImages
+                // ,onCancel: null
+            // });
+            // toolContainer.setVisible(false);
             
-            var selectDrawingObject = function() {
-                geo = null;
-				map.drawing.forEachObject(function(o) { geo = gmxAPI.merc_geometry(o.geometry); });
-                toolContainer.setVisible( geo ? true : false);
-            }
+            //var geo = null;
             
-            map.drawing.addListener('onRemove', function() {
-                setTimeout(selectDrawingObject, 0);
-            });
-            
-            map.drawing.addListener('onFinish', function(params) {
-                geo = gmxAPI.merc_geometry(params.geometry);
-                toolContainer.setVisible(true);
-            });
-            // var getSearchLayers = function(flag) {
-                // var out = [];
-                // for (var i = 0, len = map.layers.length; i < len; i++) {
-                    // var layer = map.layers[i];
-                    // if(!flag && !layer.isVisible) continue;
-                    // if(layer.properties.Quicklook || layer.properties.IsRasterCatalog) {
-                        // if(layer.properties.IsRasterCatalog) {
-                            // // установка режима просмотра растров по onClick за пределами Мин. зум КР
-                            // // аналог атрибута layer.properties['rasterView']
-                            // // gmxAPI._cmdProxy('setAPIProperties', { 'obj': layer, 'attr':{'rasterView': 'onClick'} });
-                        // }
-                        // out.push(layer.properties.name);
-                    // }
-                // }
-                // return out;
+            // var selectDrawingObject = function() {
+                // geo = null;
+				// map.drawing.forEachObject(function(o) { geo = gmxAPI.merc_geometry(o.geometry); });
+                // toolContainer.setVisible( geo ? true : false);
             // }
+            
+            // map.drawing.addListener('onRemove', function() {
+                // setTimeout(selectDrawingObject, 0);
+            // });
+            
+            // map.drawing.addListener('onFinish', function(params) {
+                // geo = gmxAPI.merc_geometry(params.geometry);
+                // toolContainer.setVisible(true);
+            // });
+            
+            var canvas = $('<div/>').css('height', '220px');
+            
+            var menu = new leftMenu();
+            menu.createWorkCanvas("aisdnd", function(){});
+            _(menu.workCanvas, [canvas[0]], [['css', 'width', '100%']]);
+            
+            var addScenesBtn = $('<span class="buttonLink">Добавить сцены по объектам</span>').click(function() {
+                layerNames.forEach(function(layerName) {
+                    map.drawing.forEachObject(function(obj) {
+                        getLayerImages(layerName, obj.geometry);
+                    })
+                })
+            }).appendTo(canvas);
+            
+            var scenesDiv = $('<div/>').appendTo(canvas);
+            var ScrollTable = gmxCore.getModule("ScrollTableControl").ScrollTable;
+            var sceneTable = new ScrollTable({height: 170});
+            var dataProvider = new ScrollTable.StaticDataProvider();
+            sceneTable.setDataProvider(dataProvider);
+            sceneTable.createTable({
+                parent: scenesDiv[0],
+                name: 'sales_scene_list',
+                fields: ['ID'],
+                fieldsWidths: ['100%'],
+                drawFunc: function(item) {
+                    return $('<tr><td>' + item.properties.Name + '</td></tr>')[0];
+                }
+            });
+            
+            var sceneCollection = new SceneCollection(selectedImagesLayer, dataProvider);
         }
     };
     gmxCore.addModule('findImages', publicInterface, {});
