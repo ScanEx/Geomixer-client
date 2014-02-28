586bf51d9e9d0558754b1e305d7f4439d10b0e99
Alexander Parshin <parshin.alexander@gmail.com>
2014-02-28 19:34:33 +0400

    Плагин HelpEmail с новой структурой левой панели

--- plugins/HelpEmailPlugin.js
+++ plugins/HelpEmailPlugin.js
@@ -14,7 +14,14 @@ var publicInterface = {
     pluginName: 'HelpEmail',
 	afterViewer: function(params)
 	{
-		var _params = $.extend({EMail: "help@kosmosnimki.ru", Message: {rus: "Что-то не работает - напишите нам в техническую поддержку!", eng: "Something works wrong? Write an e-mail to our support!"}}, params);
+		var _params = $.extend({
+            EMail: "help@kosmosnimki.ru", 
+            Message: {
+                rus: "Что-то не работает - напишите нам в техническую поддержку!", 
+                eng: "Something works wrong? Write an e-mail to our support!"
+            }
+        }, params);
+            
 		_translationsHash.addtext("rus", {
 			"HelpEmailPlugin.Message" : _params.Message.rus
 		});
@@ -24,22 +31,19 @@ var publicInterface = {
 		
 		var interval = setInterval(function(){
 			if (_queryMapLayers.buildedTree){
-				leftContentHeightDecrease = 50;
 				clearInterval(interval);
                 var sMapID = _layersTree.treeModel.getMapProperties().name;
 				var sSubject = escape("BugReport: MapID=" + sMapID + " ; Login=" + nsGmx.AuthManager.getLogin());
 				var sBody = escape("MapUrl: http://maps.kosmosnimki.ru/api/index.html?" + sMapID);
 				var sAddress = "mailto:" + _params.EMail + "?subject=" + sSubject + "&body=" + sBody; 
 				var oLink = _a([_t(_gtxt("HelpEmailPlugin.Message"))], [[ "attr", "href", sAddress]]);
-				oLink.style.textDecoration = "none"
-				var oLinkDiv = _div([oLink, _br(), _a([_t(_params.EMail)], [["attr", "href", "mailto:" + _params.EMail]])], [["attr", "align", "center"]])
-				oLinkDiv.style.position = "absolute";
+				oLink.style.textDecoration = "none";
+				var oLinkDiv = _div([oLink, _br(), _a([_t(_params.EMail)], [["attr", "href", "mailto:" + _params.EMail]])], [["attr", "align", "center"]]);
 				oLinkDiv.style.width = "325px";
-				oLinkDiv.style.bottom = "5px";
-				oLinkDiv.style.left = "13px";
 				oLinkDiv.style.border = "1px solid";
 				oLinkDiv.style.padding = "3px 5px 3px 5px";
-				_(document.getElementById("all"), [oLinkDiv]);
+				oLinkDiv.style.margin = "5px 0px 5px 13px";
+                _(document.getElementById("leftPanelFooter"), [oLinkDiv]);
 				resizeAll();
 			}
 		} , 200);
