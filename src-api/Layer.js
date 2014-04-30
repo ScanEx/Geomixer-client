bc9684d067c2b42841eb99479b0cd8b6d448338d
OriginalSin <saleks@scanex.ru>
2014-04-30 17:21:37 +0400

    LayerVersion - отсутствует то слой считаем клиентским

--- src-api/Layer.js
+++ src-api/Layer.js
@@ -1043,10 +1043,8 @@
         }
         parentObj.layers.push(obj);
         parentObj.layers[layerName] = obj;
-        if (!layer.properties.title) {
-            obj.notServer = true;
-            layer.properties.title = 'layer from client ' + layerName;
-        }
+        if (!layer.properties.LayerVersion) obj.notServer = true;
+        if (!layer.properties.title) layer.properties.title = 'layer from client ' + layerName;
         if (!layer.properties.title.match(/^\s*[0-9]+\s*$/))
             parentObj.layers[layer.properties.title] = obj;
 
