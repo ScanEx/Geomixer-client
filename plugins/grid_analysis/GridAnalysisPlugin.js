0d0e2942149e3ab987a9577852f5def2ec9a4414
Alexander Parshin <parshin.alexander@gmail.com>
2014-07-15 17:53:39 +0400

    Плагин grid analysis: работа с несколькими слоями + исправление бага при клике вне слоя

--- plugins/grid_analysis/GridAnalysisPlugin.js
+++ plugins/grid_analysis/GridAnalysisPlugin.js
@@ -1,6 +1,6 @@
 ﻿// Вспомогательный плагин для пометки просмотренных территорий в процессе дешифрирования
 // Параметры плагина:
-// * layer - ID слоя
+// * layer - ID слоёв, через запятую
 // * column - название колонки для изменения
 // * menu<N> - добавить новый пункт меню и именем, равным значению параметра и которое устанавливает значение "N" в колонке
 (function ($){
@@ -26,32 +26,43 @@ var parseMenuItems = function(params){
 var publicInterface = {
 	afterViewer: function(params, map)
     {
-        var layerName = params.layer,
+        var layerNames = params.layer.split(','),
             propName = params.column || 'done',
-            menus = parseMenuItems(params),
-            identityField = map.layers[layerName].properties.identityField;
+            menus = parseMenuItems(params);
             
         $.each(menus, function(menuName, value) {
             map.addContextMenuItem(menuName, function(lat, lng) {
-                
+                var defs = [];
                 var loading = _img(null, [['attr','src','img/loader2.gif'],['attr','savestatus','true'],['css','margin','8px 0px 0px 10px']]);
                 _($$('headerLinks'), [loading]);
                 
-                _mapHelper.searchObjectLayer(layerName, {
-                    pagesize: 1, 
-                    border: gmxAPI.merc_geometry({type: 'POINT', coordinates: [lat, lng]})
-                }).done(function(objects) {
-                    if (objects.length) {
-                        var props = {};
-                        var newValue = objects[0].properties[propName] == value ? 0 : value;
-                        props[propName] = Number(newValue);
-                        _mapHelper.modifyObjectLayer(layerName, [{
-                            id: objects[0].properties[identityField],
-                            properties: props
-                        }]).done(function() {
-                            _layersTree.showSaveStatus($$('headerLinks'));
-                        })
-                    }
+                layerNames.forEach(function(layerName) {
+                    var identityField = map.layers[layerName].properties.identityField,
+                        def = $.Deferred();
+                        
+                    defs.push(def);
+                    
+                    _mapHelper.searchObjectLayer(layerName, {
+                        pagesize: 1,
+                        border: gmxAPI.merc_geometry({type: 'POINT', coordinates: [lat, lng]})
+                    }).then(function(objects) {
+                        if (objects.length) {
+                            var props = {};
+                            var newValue = objects[0].properties[propName] == value ? 0 : value;
+                            props[propName] = Number(newValue);
+                            _mapHelper.modifyObjectLayer(layerName, [{
+                                id: objects[0].properties[identityField],
+                                properties: props
+                            }]).then(function() {
+                                def.resolve();
+                            })
+                        } else {
+                            def.resolve();
+                        }
+                    })
+                })
+                $.when.apply($, defs).then(function() {
+                    _layersTree.showSaveStatus($$('headerLinks'));
                 })
             })
         });
