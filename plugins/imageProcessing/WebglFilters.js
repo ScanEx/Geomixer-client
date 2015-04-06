e84758878199ffa9eeb5c9655d0508339f33ebc1
OriginalSin <saleks@scanex.ru>
2015-04-06 16:56:52 +0300

    Обновил плагин WebglFilters

--- plugins/imageProcessing/WebglFilters.js
+++ plugins/imageProcessing/WebglFilters.js
@@ -1,10 +1,10 @@
 ﻿(function() {
 
-    _translationsHash.addtext("rus", {
-        "WebglFilters.iconTitle" : "Показать/Скрыть контура векторных тайлов активного слоя"
+    _translationsHash.addtext('rus', {
+        'WebglFilters.title' : 'Включить/Выключить фильтры растров активного слоя'
     });
-    _translationsHash.addtext("eng", {
-        "WebglFilters.iconTitle" : "Show/Hide active vector layer tiles bounds"
+    _translationsHash.addtext('eng', {
+        'WebglFilters.title' : 'Show/Hide raster filters'
     });
 
     var publicInterface = {
@@ -23,84 +23,79 @@
                 return;
             }
 
-            //var WebGLcanvas = null;
-
             var pluginLoaded = function() {
-                var menu = null;
-                var testLayer = null;
-                var cont = null;
-                var mapListenerId = null;
-                var toolContainer = new map.ToolsContainer('addObject', {style: {padding: '0px'}});
-                var tool = toolContainer.addTool('addObject', {
-                    hint: _gtxt('WebglFilters.iconTitle'),
-                    regularStyle: {padding: '0px', display: 'list-item'},
-                    activeStyle: {backgroundColor: 'red'},
-                    regularImageUrl: _params.regularImage.search(/^https?:\/\//) !== -1 ? _params.regularImage : path + _params.regularImage,
-                    activeImageUrl:  _params.activeImage.search(/^https?:\/\//) !== -1 ? _params.activeImage : path + _params.activeImage,
-                    onClick: function() {
-                        var canvas = fx.canvas();
-                        var WebglFilters = L.gmx.WebglFilters;
-                        var div = $('<div/>');
-                        var str = '<table id="properties">'
-                            + '<tbody><tr>'
-                            + '<th>Filter:</th>'
-                            + '<td><select id="filters">'+WebglFilters.getFiltersOptions()+'</select></td>'
-                            + '</tr><tr><th>Code:</th><td><code id="codeWebgl"></code>'
-                            + '</td></tr></tbody></table>';
-    
-                        div.append(
-                            $(str)
-                        );
-                        menu = new leftMenu();
-                        menu.createWorkCanvas("WebglFiltersMenu", function(){});
-                        _(menu.workCanvas, [div[0]], [['css', 'width', '100%']]);
-                        WebglFilters.initFiltersSelector();
+                var LMap = gmxAPI._leaflet.LMap,
+                    layersByID = gmxAPI.map.layers,
+                    blm = gmxAPI.map.baseLayersManager,
+                    menu = null,
+                    testLayer = null;
+                
+                var filtersIcon = new L.Control.gmxIcon({
+                        id: 'filtersIcon', 
+                        togglable: true,
+                        className: 'leaflet-gmx-icon-sprite',
+                        regularImageUrl: _params.regularImage.search(/^https?:\/\//) !== -1 ? _params.regularImage : path + _params.regularImage,
+                        activeImageUrl:  _params.activeImage.search(/^https?:\/\//) !== -1 ? _params.activeImage : path + _params.activeImage,
+                        title: _gtxt('WebglFilters.title')
+                    }).on('statechange', function(ev) {
+                        var control = ev.target,
+                            testLayerID = null;
+                        if (control.options.isActive) {
+                            var canvas = fx.canvas(),
+                                WebglFilters = L.gmx.WebglFilters,
+                                div = $('<div/>');
+                            var str = '<table id="properties">'
+                                + '<tbody><tr>'
+                                + '<th>Filter:</th>'
+                                + '<td><select id="filters">'+WebglFilters.getFiltersOptions()+'</select></td>'
+                                + '</tr><tr><th>Code:</th><td><code id="codeWebgl"></code>'
+                                + '</td></tr></tbody></table>';
+        
+                            div.append(
+                                $(str)
+                            );
+                            menu = new leftMenu();
+                            menu.createWorkCanvas("WebglFiltersMenu", function(){});
+                            _(menu.workCanvas, [div[0]], [['css', 'width', '100%']]);
+                            WebglFilters.initFiltersSelector();
 
-                        function getActiveLayer() {
-                            var out = null;
-                            var active = $(_queryMapLayers.treeCanvas).find(".active");
-                            if (active && active[0] && active[0].parentNode.getAttribute("LayerID"))
-                            {
-                                var activeLayerName = active[0].parentNode.gmxProperties.content.properties.name;
-                                var layer = gmxAPI.map.layers[activeLayerName];
-                                var dateInterval = layer.getDateInterval();
-                                out = gmxAPI.map.layers[activeLayerName];
-                            } else {
-                                var blm = gmxAPI.map.baseLayersManager,
-                                    layers = blm.getLayers(blm.getCurrentID());
-
-                                if (layers && layers.length) {
-                                    out = gmxAPI.mapNodes[layers[0].objectId];
+                            function getActiveLayer() {
+                                var out = null;
+                                var active = $(_queryMapLayers.treeCanvas).find(".active");
+                                if (active && active[0] && active[0].parentNode.getAttribute("LayerID")) {
+                                    var activeLayerName = active[0].parentNode.gmxProperties.content.properties.name;
+                                    out = layersByID[activeLayerName];
+                                } else {
+                                    var layers = blm.getLayers(blm.getCurrentID());
+                                    if (layers && layers.length) {
+                                        out = gmxAPI.mapNodes[layers[0].objectId];
+                                    }
                                 }
+                                return out;
                             }
-                            
-                            return out;
-                        }
-                        var testLayerID = null,
-                            testLayer = getActiveLayer();
 
-                        if(testLayer) {
-                            L.gmx.WebglFilters.callback = function() {
-                                testLayer.repaint();
-                            };
-                            testLayer.setImageProcessingHook(function(img, attrs) {
-                                var texture = canvas.texture(img);
-                                canvas.draw(texture);
-                                L.gmx.WebglFilters.code(canvas).update();
-                                var canvasOut = document.createElement('canvas');
-                                canvasOut.width = canvasOut.height = 256;
-                                var ctx = canvasOut.getContext('2d');
-                                ctx.drawImage(canvas, 0, 0);
-                                return canvasOut;
-                            }, 'anonymous') ; 
+                            testLayer = getActiveLayer();
+                            if(testLayer) {
+                                L.gmx.WebglFilters.callback = function() {
+                                    testLayer.repaint();
+                                };
+                                testLayer.setImageProcessingHook(function(img, attrs) {
+                                    var texture = canvas.texture(img);
+                                    canvas.draw(texture);
+                                    L.gmx.WebglFilters.code(canvas).update();
+                                    var canvasOut = document.createElement('canvas');
+                                    canvasOut.width = canvasOut.height = 256;
+                                    var ctx = canvasOut.getContext('2d');
+                                    ctx.drawImage(canvas, 0, 0);
+                                    return canvasOut;
+                                }, 'anonymous') ; 
+                            }
+                        } else {
+                            if(menu && menu.workCanvas) menu.workCanvas.parentNode.removeNode(menu.workCanvas);
+                            if(testLayer) testLayer.removeImageProcessingHook();
                         }
-                    },
-                    onCancel: function(){
-                        if(menu && menu.workCanvas) menu.workCanvas.parentNode.removeNode(menu.workCanvas);
-                        if(testLayer) testLayer.removeImageProcessingHook();
-                        
-                    }
-                });
+                    });
+                LMap.addControl(filtersIcon);
             };
             gmxAPI.loadCSS(path + 'demo.css');
             var arr = [
