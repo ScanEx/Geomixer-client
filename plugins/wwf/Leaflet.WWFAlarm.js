c17214c676c19b0706d472bef3cfe47576d06a67
Alexander Parshin <parshin.alexander@gmail.com>
2015-05-08 10:45:35 +0300

    Плагин WWF: отдельный плагин для Leaflet

--- plugins/wwf/Leaflet.WWFAlarm.js
+++ plugins/wwf/Leaflet.WWFAlarm.js
@@ -2,7 +2,9 @@
     options: {
         async: true
     },
-    drawTile: function(canvas, point, zoom) {
+    _tileTemplate: 'http://maps.kosmosnimki.ru/TileService.ashx?request=gettile&layername={layerID}&srs=EPSG:3857&z={z}&x={x}&y={y}&format=png&Map={mapID}',
+    
+    drawTile: function(canvas, tilePoint, zoom) {
         var img = new Image();
         
         img.onload = function() {
@@ -19,7 +21,7 @@
             for (var p = 0; p < 256*256*4; p += 4) {
                 var v = data[p] + data[p+1];
                 if (v >= minVal && v <= maxVal) {
-                    data[p] = Math.floor((365-v)*coeff);
+                    data[p+0] = Math.floor((365-v)*coeff);
                     data[p+1] = Math.floor(v*coeff);
                 } else {
                     data[p+3] = 0;
@@ -35,6 +37,20 @@
             this.tileDrawn(canvas);
         }.bind(this);
         
-        img.src = '';
+        img.crossOrigin = 'Anonymous';
+        
+        img.src = L.Util.template(this._tileTemplate, {
+            x: tilePoint.x,
+            y: tilePoint.y,
+            z: zoom,
+            layerID: 'C53699CEACAF4D8AB0ACF1A4D152D85A',
+            mapID: '1D116AC56D694C0B8CDCA87C06D1961A'
+        });
+    },
+    setDateInterval: function(dateBegin, dateEnd) {
+        this._minDate = dateBegin;
+        this._maxDate = dateEnd;
+        this.redraw();
+        return this;
     }
 })
