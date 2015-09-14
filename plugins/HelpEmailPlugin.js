4ba1cbcbe9436d2fd07066f85b0d8c2e4ab5adae
Alexander Parshin <parshin.alexander@gmail.com>
2015-09-14 11:33:51 +0300

    HelpEmailPlugin: информационное сообщение просто текстом, без ссылки на почту

--- plugins/HelpEmailPlugin.js
+++ plugins/HelpEmailPlugin.js
@@ -1,20 +1,9 @@
-﻿
-(function ($){
-/*
-_translationsHash.addtext("rus", {
-	"HelpEmailPlugin.Message" : "Что-то не работает - напишите нам в техническую поддержку!"
-});
-
-_translationsHash.addtext("eng", {
-	"HelpEmailPlugin.Message" : "Something works wrong? Write an e-mail to our support!"
-});
- */
- 
+﻿(function ($){
 var publicInterface = {
     pluginName: 'HelpEmail',
-	afterViewer: function(params)
-	{
-		var _params = $.extend({
+    afterViewer: function(params)
+    {
+        var _params = $.extend({
             EMail: "help@kosmosnimki.ru", 
             Message: {
                 rus: "Что-то не работает - напишите нам в техническую поддержку!", 
@@ -22,34 +11,26 @@ var publicInterface = {
             }
         }, params);
             
-		_translationsHash.addtext("rus", {
-			"HelpEmailPlugin.Message" : _params.Message.rus
-		});
-		_translationsHash.addtext("eng", {
-			"HelpEmailPlugin.Message" : _params.Message.eng
-		});
-		
-		var interval = setInterval(function(){
-			if (_queryMapLayers.buildedTree){
-				clearInterval(interval);
-                var sMapID = _layersTree.treeModel.getMapProperties().name;
-				var sSubject = escape("BugReport: MapID=" + sMapID + " ; Login=" + nsGmx.AuthManager.getLogin());
-				var sBody = escape("MapUrl: http://maps.kosmosnimki.ru/api/index.html?" + sMapID);
-				var sAddress = "mailto:" + _params.EMail + "?subject=" + sSubject + "&body=" + sBody; 
-				var oLink = _a([_t(_gtxt("HelpEmailPlugin.Message"))], [[ "attr", "href", sAddress]]);
-				oLink.style.textDecoration = "none";
-				var oLinkDiv = _div([oLink, _br(), _a([_t(_params.EMail)], [["attr", "href", "mailto:" + _params.EMail]])], [["attr", "align", "center"]]);
-				oLinkDiv.style.width = "325px";
-				oLinkDiv.style.border = "1px solid";
-				oLinkDiv.style.padding = "3px 5px 3px 5px";
-				oLinkDiv.style.margin = "5px 0px 5px 13px";
-                _(document.getElementById("leftPanelFooter"), [oLinkDiv]);
-				resizeAll();
-			}
-		} , 200);
-	}
+        _translationsHash.addtext("rus", {
+            "HelpEmailPlugin.Message" : _params.Message.rus
+        });
+        _translationsHash.addtext("eng", {
+            "HelpEmailPlugin.Message" : _params.Message.eng
+        });
+        
+        _queryMapLayers.loadDeferred.then(function() {
+            var oLink = _t(_gtxt("HelpEmailPlugin.Message"));
+            var oLinkDiv = _div([oLink, _br(), _a([_t(_params.EMail)], [["attr", "href", "mailto:" + _params.EMail]])], [["attr", "align", "center"]]);
+            oLinkDiv.style.width = "325px";
+            oLinkDiv.style.border = "1px solid";
+            oLinkDiv.style.padding = "3px 5px 3px 5px";
+            oLinkDiv.style.margin = "5px 0px 5px 13px";
+            _(document.getElementById("leftPanelFooter"), [oLinkDiv]);
+            resizeAll();
+        });
+    }
 }
 
 gmxCore.addModule("HelpEmailPlugin", publicInterface);
 
-})(jQuery, globalFlashMap)
+})(jQuery);
