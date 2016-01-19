7cd304b3ce04900d7304c83cbac7263627629282
Alexander Parshin <parshin.alexander@gmail.com>
2016-01-19 12:15:45 +0300

    GFW plugin: не используем TileVector.Canvas.Mercator

--- plugins/gfw/GFWPlugin.js
+++ plugins/gfw/GFWPlugin.js
@@ -2,8 +2,6 @@
 */
 (function ($){
 
-var GFWLayer = null;
-
 var GFWSlider = L.Control.extend({
     includes: L.Mixin.Events,
     _yearBegin: 2001,
@@ -71,59 +69,56 @@ var GFWSlider = L.Control.extend({
     }
 });
 
-function defineClass() {
-    GFWLayer = L.TileLayer.Canvas.Mercator.extend({
-        options: {
-            async: true, 
-            tilesCRS: L.CRS.EPSG3857,
-            attribution: '<a href="http://glad.umd.edu/"> Hansen|UMD|Google|USGS|NASA </a>'
-        },
-        _yearBegin: 2001,
-        _yearEnd: 2015,
-        _drawLayer: function(img, ctx, z) {
-            var imgData = ctx.getImageData(0, 0, 256, 256),
-                data = imgData.data,
-                exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1;
-                
-            for (var i = 0; i < 256; ++i) {
-                for (var j = 0; j < 256; ++j) {
-                    var pixelPos = (j * 256 + i) * 4,
-                        yearLoss = 2000 + data[pixelPos + 2],
-                        intensity = data[pixelPos],
-                        scale = Math.pow(intensity/256, exp) * 256;
+var GFWLayer = L.TileLayer.Canvas.extend({
+    options: {
+        async: true,
+        attribution: '<a href="http://glad.umd.edu/"> Hansen|UMD|Google|USGS|NASA </a>'
+    },
+    _yearBegin: 2001,
+    _yearEnd: 2015,
+    _drawLayer: function(img, ctx, z) {
+        var imgData = ctx.getImageData(0, 0, 256, 256),
+            data = imgData.data,
+            exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1;
+            
+        for (var i = 0; i < 256; ++i) {
+            for (var j = 0; j < 256; ++j) {
+                var pixelPos = (j * 256 + i) * 4,
+                    yearLoss = 2000 + data[pixelPos + 2],
+                    intensity = data[pixelPos],
+                    scale = Math.pow(intensity/256, exp) * 256;
 
-                    if (yearLoss >= this._yearBegin && yearLoss < this._yearEnd) {
-                        data[pixelPos] = 220;
-                        data[pixelPos + 1] = (72 - z) + 102 - (3 * scale / z);
-                        data[pixelPos + 2] = (33 - z) + 153 - ((intensity) / z);
-                        data[pixelPos + 3] = z < 13 ? scale : intensity;
-                    } else {
-                        data[pixelPos + 3] = 0;
-                    }
+                if (yearLoss >= this._yearBegin && yearLoss < this._yearEnd) {
+                    data[pixelPos] = 220;
+                    data[pixelPos + 1] = (72 - z) + 102 - (3 * scale / z);
+                    data[pixelPos + 2] = (33 - z) + 153 - ((intensity) / z);
+                    data[pixelPos + 3] = z < 13 ? scale : intensity;
+                } else {
+                    data[pixelPos + 3] = 0;
                 }
             }
-            
-            ctx.putImageData(imgData, 0, 0);
-        },
-        drawTile: function(canvas, tilePoint, zoom) {
-            var img = new Image();
-            img.crossOrigin = "Anonymous";
-            img.onload = function() {
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0, 256, 256);
-                this._drawLayer(img, ctx, zoom);
-                this.tileDrawn(canvas);
-            }.bind(this);
-            
-            img.src = 'http://storage.googleapis.com/earthenginepartners-hansen/tiles/gfw2015/loss_tree_year_25/' + zoom + '/' + tilePoint.x + '/' + tilePoint.y + '.png';
-        },
-        setYearInterval: function(yearBegin, yearEnd) {
-            this._yearBegin = yearBegin;
-            this._yearEnd = yearEnd;
-            this.redraw();
         }
-    })
-}
+        
+        ctx.putImageData(imgData, 0, 0);
+    },
+    drawTile: function(canvas, tilePoint, zoom) {
+        var img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.onload = function() {
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, 256, 256);
+            this._drawLayer(img, ctx, zoom);
+            this.tileDrawn(canvas);
+        }.bind(this);
+        
+        img.src = 'http://storage.googleapis.com/earthenginepartners-hansen/tiles/gfw2015/loss_tree_year_25/' + zoom + '/' + tilePoint.x + '/' + tilePoint.y + '.png';
+    },
+    setYearInterval: function(yearBegin, yearEnd) {
+        this._yearBegin = yearBegin;
+        this._yearEnd = yearEnd;
+        this.redraw();
+    }
+})
 
 var GeoMixerGFWLayer = function() {
     this._layer = new GFWLayer();
@@ -214,12 +209,6 @@ var publicInterface = {
 }
 
 gmxCore.addModule('GFWPlugin', publicInterface, {
-    init: function(params, path) {
-        return gmxCore.loadScriptWithCheck([{
-            check: function() {return L.TileLayer.Canvas.Mercator; },
-            script: path + 'TileLayer.Mercator.js'
-        }]).then(defineClass);
-    },
     css: 'GFWPlugin.css'
 });
 
