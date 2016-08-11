7c16a5f65985c4e0a98badd41f28c73b66b0f87b
ggolikov <gr.golikov@gmail.com>
2016-08-11 14:38:58 +0300

    добавлена поддержка shared resources в диалоге свойств группы пользователей

--- UserGroupWidget.js
+++ UserGroupWidget.js
@@ -16,6 +16,11 @@ nsGmx.Translations.addText('rus', {uglw: {
         title: 'Удалить группу',
         ok: 'Ok',
         cancel: 'Отмена'
+    },
+    shareLabel: 'Права на ресурсы внутри группы:',
+    share: {
+        'public': 'общие',
+        'private': 'раздельные'
     }
 }});
 
@@ -33,6 +38,11 @@ nsGmx.Translations.addText('eng', {uglw: {
         title: 'Delete group',
         ok: 'Ok',
         cancel: 'Cancel'
+    },
+    shareLabel: 'Sharing resources among members:',
+    share: {
+        'public': 'selectively',
+        'private': 'by default'
     }
 }});
 
@@ -68,50 +78,50 @@ var GroupListView = Backbone.View.extend({
         var rawAttributes = this.model.map(function(user) {
             return user.attributes;
         });
-        
+
         this.$el.empty().append($(this.template({users: rawAttributes})));
-        
+
         this.$el.find('.uglw-group-name').click(function() {
             var groupID = Number($(this).data('groupid'));
-                
+
             var groupSecurity = new UserGroupSecurity();
-            
+
             $(groupSecurity).on('savedone', function() {
                 _this.trigger('needupdate');
             });
-            
+
             groupSecurity.propertyValue = groupID;
             groupSecurity.title = _this.model.get(groupID).get('Nickname');
             groupSecurity.getSecurityFromServer(groupID).then(function(res) {
                 groupSecurity.createSecurityDialog(res);
                 $(groupSecurity._dialogDiv).find('.security-props-title').focus();
             })
-            
+
         });
-        
+
         var popoverUI = $(Handlebars.compile('<div>' +
             '<div>{{i "uglw.popover.title"}}?</div>' +
             '<div class="uglw-popover-buttons">' +
                 '<button class="uglw-popover-ok">{{i "uglw.popover.ok"}}</button>' +
-                '<button class="uglw-popover-cancel">{{i "uglw.popover.cancel"}}</button>' + 
+                '<button class="uglw-popover-cancel">{{i "uglw.popover.cancel"}}</button>' +
             '</div>' +
         '</div>')());
-        
+
         popoverUI.find('.uglw-popover-ok').click(function() {
             sendCrossDomainJSONRequest(serverBase + 'User/DeleteGroup?GroupID=' + _this._popoverGroupID, function(response) {
                 if (!parseResponse(response)) {
                     return;
                 };
-                
+
                 _this.trigger('needupdate');
                 _this.$el.find('.gmx-icon-recycle').popover('hide');
             });
         });
-        
+
         popoverUI.find('.uglw-popover-cancel').click(function() {
             _this.$el.find('.gmx-icon-recycle').popover('hide');
         });
-        
+
         this.$el.find('.gmx-icon-recycle').click(function() {
             var groupID = Number($(this).data('groupid'));
             _this._popoverGroupID = groupID;
@@ -128,25 +138,25 @@ var GroupListView = Backbone.View.extend({
         });
     }
 });
-    
+
 nsGmx.UserGroupListWidget = function(container) {
     var ui = $(nsGmx.UserGroupListWidget._mainTemplate()).appendTo(container);
-    
+
     var groupList = new GroupList();
-    
+
     var listView = new GroupListView({
         el: ui.find('.uglw-list-placeholder')[0],
         model: groupList
     });
-    
+
     var refilterUsers = function() {
         nsGmx.security.findUsers(ui.find('.uglw-filter-input').val(), {type: 'Group'}).then(function(users) {
             groupList.reset(users);
         })
     }
-    
+
     listView.on('needupdate', refilterUsers);
-    
+
     var lastValue = null;
     ui.find('.uglw-filter-input').on('keyup change', function(){
         if (lastValue !== this.value) {
@@ -154,12 +164,12 @@ nsGmx.UserGroupListWidget = function(container) {
             refilterUsers();
         }
     });
-    
+
     ui.find('.uglw-filter-input').change();
-    
+
     ui.find('.uglw-add-icon').click(function() {
         var groupSecurity = new UserGroupSecurity();
-        
+
         groupSecurity._save = function() {
             var _this = this,
                 si = this._securityInfo.SecurityInfo;
@@ -184,19 +194,19 @@ nsGmx.UserGroupListWidget = function(container) {
                     if (!parseResponse(response)) {
                         return;
                     }
-                    
+
                     $(groupSecurity._dialogDiv).dialog('close');
                     refilterUsers();
                 }
             )
         }
-        
+
         groupSecurity.createSecurityDialog({
             SecurityInfo: {
                 Users: []
             }
         }, {showOwner: false});
-        
+
         $(groupSecurity._dialogDiv).find('.security-props-title').focus();
     });
 }
@@ -213,10 +223,10 @@ var UserGroupSecurity = function()
 {
     this.getSecurityName = "User/GetUserGroupSecurity";
     this.updateSecurityName = "User/UpdateUserGroupSecurity";
-    
+
     this.propertyName = "UserID";
     this.dialogTitle = "Состав группы [value0]";
-    
+
     this.accessTypes = ['no', 'view', 'edit'];
 }
 
@@ -226,7 +236,7 @@ UserGroupSecurity.prototype.constructor = UserGroupSecurity;
 UserGroupSecurity.prototype.saveCustomParams = function() {
     var nicknameInput = this._ui.find('.security-props-title'),
         nickname = nicknameInput.val();
-    
+
     if (nickname) {
         this._securityInfo.SecurityInfo.Nickname = nickname;
         this._securityInfo.SecurityInfo.Description = this._ui.find('.security-props-description').val();
@@ -234,6 +244,10 @@ UserGroupSecurity.prototype.saveCustomParams = function() {
         inputError(nicknameInput[0]);
         return true;
     }
+
+    var publicCheckbox = this._ui.find('#publicRights')[0];
+
+    this._securityInfo.SecurityInfo.SharedResources = publicCheckbox.checked;
 }
 
 UserGroupSecurity.prototype.addCustomUI = function(ui, securityInfo) {
@@ -241,12 +255,22 @@ UserGroupSecurity.prototype.addCustomUI = function(ui, securityInfo) {
         '<table class="security-props">' +
             '<tr><td>{{i "uglw.groupProps.title"}}:</td><td><input class="security-props-title inputStyle" value="{{Nickname}}"></td></tr>' +
             '<tr><td>{{i "uglw.groupProps.description"}}:</td><td><input class="security-props-description inputStyle" value="{{Description}}"></td></tr>' +
-        '</table>'
+        '</table>' +
+        '<div class="security-policy-control">' +
+            '<span class="security-policy-label">{{i "uglw.shareLabel"}}</span>' +
+            '<span class="security-policy-radiogroup">' +
+                '<input id="privateRights" class="policy-checkbox" type="radio" name="groupEditingPolicy" {{#unless SharedResources}} checked {{/unless}}/>' +
+                '<label class="security-policy-label" for="privateRights">{{i "uglw.share.private"}}</label>' +
+                '<input id="publicRights" class="policy-checkbox" type="radio" name="groupEditingPolicy" {{#if SharedResources}} checked {{/if}}/>' +
+                '<label class="security-policy-label" for="publicRights">{{i "uglw.share.public"}}</label>' +
+            '</span>' +
+        '</div>'
     );
 
     $(propsTemplate({
         Nickname: securityInfo.Nickname,
-        Description: securityInfo.Description
+        Description: securityInfo.Description,
+        SharedResources: securityInfo.SharedResources
     })).appendTo(ui.find('.security-custom-ui'));
 }
 
