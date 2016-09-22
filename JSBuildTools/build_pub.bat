4b90e8859460c13953f2ba8c68280d8f87d907ff
ggolikov <gr.golikov@gmail.com>
2016-09-22 17:08:44 +0300

    copy dist files after build-dev

--- JSBuildTools/build_pub.bat
+++ JSBuildTools/build_pub.bat
@@ -1,1 +1,15 @@
-gulp gmx-pub
+call gulp gmx-pub
+
+set destination="..\..\Geomixer-dist"
+
+xcopy .. %destination%
+
+robocopy ..\common_components\dist %destination%\common_components\dist /S
+
+robocopy ..\dist %destination%\dist /S
+
+robocopy ..\img %destination%\img /S
+
+robocopy ..\plugins\external %destination%\plugins\external /S
+
+robocopy ..\src\GridPlugin %destination%\src\GridPlugin /S
