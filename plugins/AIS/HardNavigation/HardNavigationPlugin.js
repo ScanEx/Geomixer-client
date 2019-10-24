c6de69485381730097b3865069b210dd9ae68a5a
zakharov0 <zakharov@scanex.ru>
2019-10-24 19:53:35 +0300

    Draw or select

--- plugins/AIS/HardNavigation/HardNavigationPlugin.js
+++ plugins/AIS/HardNavigation/HardNavigationPlugin.js
@@ -328,8 +328,123 @@
 	            return this.frame.find('.footer')[0].getBoundingClientRect().height;
 	        }
 	    });
+	
+	    this.frame.find('.but.choose').on('click', _copyRegion.bind(this));
+	    this.frame.find('.but.create').on('click', _createRegion.bind(this));
+	},
+	    _listeners = {},
+	    _layerClickHandler = function _layerClickHandler(event) {
+	    var layer = event.target,
+	        props = layer.getGmxProperties(),
+	        id = event.gmx.properties[props.identityField];
+	
+	    layer.bringToTopItem(id);
+	    //new nsGmx.EditObjectControl(props.name, id, { event: event });
+	    sendCrossDomainJSONRequest(serverBase + 'VectorLayer/Search.ashx?WrapStyle=func&layer=' + props.name + '&page=0&pagesize=1&orderby=' + props.identityField + '&geometry=true&query=[' + props.identityField + ']=' + id, function (response) {
+	        if (_stateUI == 'copy_region') {
+	            if (response.Status && response.Status.toLowerCase() == 'ok') {
+	                var i = response.Result.fields.indexOf('geomixergeojson');
+	                //console.log(L.gmxUtil.geometryToGeoJSON(response.Result.values[0][i], true), `>>${i}`, this, props.name);
+	                var obj = nsGmx.leafletMap.gmxDrawing.addGeoJSON(L.gmxUtil.geometryToGeoJSON(response.Result.values[0][i], true));
+	            } else console.log(response);
+	            this.frame.find('.but.choose').click();
+	        }
+	    }.bind(this));
+	    return true;
+	},
+	    _copyRegion = function _copyRegion() {
+	    var but = this.frame.find('.but.choose');
+	    if (_stateUI != 'copy_region') {
+	        if (_stateUI != '') this.frame.find('.but.create').click();
+	        _stateUI = 'copy_region';
+	        but.addClass('active');
+	
+	        for (var iL = 0; iL < nsGmx.gmxMap.layers.length; iL++) {
+	            var layer = nsGmx.gmxMap.layers[iL],
+	                props = layer.getGmxProperties();
+	
+	            if (layer.disableFlip && layer.disablePopup) {
+	                layer.disableFlip();
+	                layer.disablePopup();
+	            }
+	
+	            if (layer instanceof L.gmx.VectorLayer && props.GeometryType.toLowerCase() == 'polygon') {
+	                _listeners[props.name] = _layerClickHandler;
+	                layer.on('click', _listeners[props.name].bind(this));
+	            }
+	        }
+	    } else {
+	        _stateUI = '';
+	        but.removeClass('active');
+	        nsGmx.gmxMap.layers.forEach(function (layer) {
+	            if (layer.disableFlip && layer.disablePopup) {
+	                layer.enableFlip();
+	                layer.enablePopup();
+	            }
+	        });
+	        for (var layerName in _listeners) {
+	            nsGmx.gmxMap.layersByID[layerName].off('click', _listeners[layerName]);
+	            delete _listeners[layerName];
+	        }
+	    }
+	},
+	    _onDrawStop = function _onDrawStop(e) {
+	    var gj = e.object.toGeoJSON();
+	    console.log(gj.geometry);
+	    //nsGmx.leafletMap._container.style.cursor='pointer'; 
+	    //nsGmx.leafletMap.gmxDrawing.create('Polygon'); 
+	    if (_stateUI == 'create_region') this.frame.find('.but.create').click();
+	},
+	    _createRegion = function _createRegion() {
+	    var but = this.frame.find('.but.create');
+	
+	    if (_stateUI != 'create_region') {
+	        if (_stateUI != '') this.frame.find('.but.choose').click();
+	        _stateUI = 'create_region';
+	        but.addClass('active');
+	        nsGmx.leafletMap.gmxDrawing.on('drawstop', _onDrawStop.bind(this));
+	
+	        nsGmx.gmxMap.layers.forEach(function (layer) {
+	            if (layer.disableFlip && layer.disablePopup) {
+	                layer.disableFlip();
+	                layer.disablePopup();
+	            }
+	        });
+	
+	        nsGmx.leafletMap._container.style.cursor = 'pointer';
+	        nsGmx.leafletMap.gmxDrawing.create('Polygon');
+	    } else {
+	        _stateUI = '';
+	        but.removeClass('active');
+	        nsGmx.leafletMap._container.style.cursor = '';
+	        nsGmx.leafletMap.gmxDrawing.off('drawstop', _onDrawStop);
+	
+	        nsGmx.gmxMap.layers.forEach(function (layer) {
+	            if (layer.disableFlip && layer.disablePopup) {
+	                layer.enableFlip();
+	                layer.enablePopup();
+	            }
+	        });
+	
+	        nsGmx.leafletMap.gmxDrawing.clearCreate();
+	    }
+	},
+	    _addRegion = function _addRegion() {
+	    var _this = this;
+	
+	    this.inProgress(true);
+	    var obj = nsGmx.leafletMap.gmxDrawing.addGeoJSON({ coordinates: [[[170.72753903711163, 60.844910986045214], [168.66210900000357, 60.73768601038719], [169.2333979948311, 61.17503299842439], [169.2333979948311, 61.17503299842439], [170.07934598946287, 61.14986199628548], [170.72753903711163, 60.844910986045214]]],
+	        type: "Polygon" });
+	    _mapHelper.modifyObjectLayer('FF3D4FD4291040BA9A6139EEE2CE3D23', [{ properties: { 'Name': 'Name' }, geometry: { type: 'Polygon', coordinates: [[[19005302.71, 8552947.37], [18775380.09, 8528526.66], [18838975.69, 8628653.04], [18838975.69, 8628653.04], [18933146.19, 8622852.76], [19005302.71, 8552947.37]]] } }]).done(function () {
+	        nsGmx.leafletMap.gmxDrawing.remove(obj[0]);
+	        setTimeout(function () {
+	            nsGmx.gmxMap.layersByID['FF3D4FD4291040BA9A6139EEE2CE3D23']._gmx._chkVersion();
+	            _this.inProgress(false);
+	        }, 5000);
+	    });
 	},
 	    _clean = function _clean() {};
+	var _stateUI = '';
 	
 	MyCollectionView.prototype = Object.create(BaseView.prototype);
 	
